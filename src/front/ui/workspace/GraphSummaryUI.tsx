4a3d28a9fc8607daea2e5336f79484cff580fa8e
rvion <vion.remi@gmail.com>
2023-06-03 00:21:54 +0200

    properly re-use values

---
+++ src/front/ui/workspace/GraphSummaryUI.tsx
@@ -0,0 +1,25 @@
+import type { GraphL } from 'src/models/Graph'
+import { observer } from 'mobx-react-lite'
+import { Panel } from 'rsuite'
+import { NodeRefUI } from '../NodeRefUI'
+import { CustomNodeFlow } from '../graph/Graph2UI'
+
+export const GraphSummaryUI = observer(function GraphSummaryUI_(p: { graph: GraphL }) {
+    const graph = p.graph
+    return (
+        <>
+            <Panel className='graph-container self-start w-48'>
+                {graph.size === 0 && <div>Empty Graph</div>}
+                <CustomNodeFlow />
+            </Panel>
+            <div>
+                {graph.nodes.map((n, ix) => (
+                    <div key={n.uid} className='flex'>
+                        <NodeRefUI label={ix.toString()} node={n} />
+                        {n.$schema.nameInCushy}
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+})
--- src/front/ui/workspace/GraphUI.tsx
+++ src/front/ui/workspace/GraphUI.tsx
@@ -2,31 +2,10 @@ import type { GraphL } from 'src/models/Graph'
 
 import { observer } from 'mobx-react-lite'
 import { Fragment } from 'react'
-import { Divider, Panel } from 'rsuite'
-import { NodeRefUI } from '../NodeRefUI'
-import { CustomNodeFlow } from '../graph/Graph2UI'
+import { Divider } from 'rsuite'
 import { ActionUI } from '../widgets/ActionUI'
 import { StepUI } from './StepUI'
-
-export const GraphSummaryUI = observer(function GraphSummaryUI_(p: { graph: GraphL }) {
-    const graph = p.graph
-    return (
-        <>
-            <Panel className='graph-container self-start w-48'>
-                {graph.size === 0 && <div>Empty Graph</div>}
-                <CustomNodeFlow />
-            </Panel>
-            <div>
-                {graph.nodes.map((n, ix) => (
-                    <div key={n.uid} className='flex'>
-                        <NodeRefUI label={ix.toString()} node={n} />
-                        {n.$schema.nameInCushy}
-                    </div>
-                ))}
-            </div>
-        </>
-    )
-})
+import { GraphSummaryUI } from './GraphSummaryUI'
 
 export const GraphUI = observer(function GraphUI_(p: { graph: GraphL; depth: number }) {
     const graph = p.graph
@@ -36,8 +15,10 @@ export const GraphUI = observer(function GraphUI_(p: { graph: GraphL; depth: num
     return (
         <Fragment>
             <div className='flex items-baseline'>
+                {/* depth */}
                 <div className='mr-1'>#{p.depth}</div>
-                {/* {graph.childSteps.items.length === 0 ? <ActionSuggestionUI action={action0} /> : null} */}
+
+                {/* branches */}
                 {graph.childSteps.map((step) => {
                     const isSelected = step.id === next?.id
                     return (
@@ -45,18 +26,30 @@ export const GraphUI = observer(function GraphUI_(p: { graph: GraphL; depth: num
                             key={step.id}
                             className='p-1 step-container cursor-pointer'
                             style={{ backgroundColor: isSelected ? '#797979' : undefined }}
-                            onClick={() => graph.update({ nextStepID: step.id })}
+                            onClick={() => {
+                                graph.update({ nextStepID: step.id })
+                                action0.update({
+                                    toolID: step.tool.id,
+                                    params: step.data.params,
+                                })
+                            }}
                         >
                             <div>{step.tool.item?.data.name}</div>
                         </div>
                     )
                 })}
             </div>
+
             <div className='flex gap-2 items-baseline'>
+                {/* action form */}
                 <ActionUI key={action0.id} action={action0} />
+
+                {/* input summary */}
                 <GraphSummaryUI graph={graph} />
             </div>
+
             <Divider />
+            {/* child */}
             {next && <StepUI key={next.id} step={next} depth={p.depth + 1} />}
         </Fragment>
     )
